656edf9b797acbce7bb3f35ceaf97fe1eab260ae
reut-l <ll.reut@gmail.com>
2021-08-02 09:24:37 +0300

    Adjusments for deployment

--- src/apis/appServer.js
+++ src/apis/appServer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/api/v1',
+  baseURL: 'https://damp-retreat-28399.herokuapp.com/api/v1',
   withCredentials: true,
 });
 
